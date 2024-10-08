b1a15d0071195e8f198f656213037d3a3b0bebf2
William Stroff <william.stroff@elev.ga.ntig.se>
2024-10-08 12:40:29 +0200

    Added loging and comments to Apikeys.py Added comments to Main.py Added readme.md for general information

--- Apikeys.py
+++ Apikeys.py
@@ -1,7 +1,7 @@
 class DB():
-    def __init__(self) -> None:
-        self.Name = "Temp" #FIXME
-        self.Host_IP = "Temp" #FIXME
-        self.Host_Port = "Temp" #FIXME
-        self.User_Name = "Temp" #FIXME
-        self.User_Pass = "Temp" #FIXME
+    def __init__(self):
+        self.Name = "itglue" #Database name
+        self.Host_IP = "10.0.2.15" #Ip address of the database server
+        self.Host_Port = "5432" #The port to connect to the database
+        self.User_Name = "root" #Username
+        self.User_Pass = "root" #Password
--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
-import os
-import time
-import asyncio
-import asyncpg
+import os #Import os to use in clearing the terminal
+import time #Import time to use sleep and time printing
+import asyncio #Import sync libary
+import asyncpg #Database connection thingy ma jig
 
-from Apikeys import DB
+from Apikeys import DB #Get the database class with log in information
 
-from datetime import datetime, timezone
-from asyncpg.pool import create_pool
+from datetime import datetime, timezone #Import timezone and datetime
+from asyncpg.pool import create_pool #Import the database connection function
 
+#Main
 async def Main():
-    try:
+    try: #Tries the thing, if it fails it prints out an error
+        #Connect to the database
         app_pool = await asyncpg.create_pool(
             database= DB.Name,
             host= DB.Host_IP,
@@ -18,6 +20,8 @@ async def Main():
             password= DB.User_Pass
             )
     except:
+        #Exit the program and print the error
         exit(f'Failed to connect to DB, please try again.\nERROR: 418 "Im a teapot"')
 
+#Run main function using sync
 asyncio.run(Main())
