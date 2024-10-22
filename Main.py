053a07ba5e26656afb902042e2e4f9d1205bb245
William Stroff <william.stroff@elev.ga.ntig.se>
2024-10-22 08:59:29 +0200

    idk

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import os #Import os to use in clearing the terminal
 import time #Import time to use sleep and time printing
 import asyncio #Import sync libary
 import asyncpg #Database connection thingy ma jig
+import tkinter as tk
 
 from Apikeys import DB #Get the database class with log in information
 
@@ -10,18 +11,18 @@ from asyncpg.pool import create_pool #Import the database connection function
 
 #Main
 async def Main():
-    try: #Tries the thing, if it fails it prints out an error
-        #Connect to the database
-        app_pool = await asyncpg.create_pool(
-            database= DB.Name,
-            host= DB.Host_IP,
-            port= DB.Host_Port,
-            user= DB.User_Name,
-            password= DB.User_Pass
-            )
-    except:
-        #Exit the program and print the error
-        exit(f'Failed to connect to DB, please try again.\nERROR: 418 "Im a teapot"')
+     try: #Tries the thing, if it fails it prints out an error
+         #Connect to the database
+         app_pool = await asyncpg.create_pool(
+             database= DB.Name,
+             host= DB.Host_IP,
+             port= DB.Host_Port,
+             user= DB.User_Name,
+             password= DB.User_Pass
+             )
+     except:
+         #Exit the program and print the error
+         exit(f'Failed to connect to DB, please try again.\nERROR: 418 "Im a teapot"')
 
 #Run main function using sync
 asyncio.run(Main())
