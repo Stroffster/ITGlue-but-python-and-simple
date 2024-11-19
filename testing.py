c9282fd457c602f5caac6ddc078677a002007638
William Stroff <william.stroff@elev.ga.ntig.se>
2024-11-19 14:47:48 +0100

    deleted file

--- testing.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import tkinter as tk
-
-def load_asset(path):
-    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    assets = os.path.join(base, "assets")
-    return os.path.join(assets, path)
